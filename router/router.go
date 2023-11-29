209d2d90f39b48f60bb235477490297184ab0572
andresds <andre.souza@asapcred.com.br>
2023-11-28 22:46:21 -0300

    ✨ feat(router): routes add routes logic

--- router/router.go
+++ router/router.go
@@ -2,12 +2,11 @@ package router
 
 import "github.com/gin-gonic/gin" // usei o comando: go tidy para baixar as dependencias
 
-func Initialize() { // função com letra maiuscula é exportada automaticamente
+func Initialize() { 
+	// initialize the router
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"mensagem": "andré",
-		})
-	})
+	//initialize the routes
+	initializeRoutes(router)
+	
 	router.Run(":8080") // listen and serve on
 }
